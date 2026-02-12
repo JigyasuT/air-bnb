28b3d15f33271ea8ed365205b8a8296fe8c6490a
Jigyasu Tiwari <jigyasuti@gmail.com>
2026-02-12 09:32:44 +0530

    Update server URL to production endpoint

--- frontend/src/Context/AuthContext.jsx
+++ frontend/src/Context/AuthContext.jsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState } from "react";
 export const authDataContext = createContext();
 const AuthContext = ({ children }) => {
-  let serverUrl = "http://localhost:8000";
+  let serverUrl = "https://air-bnb-back.onrender.com";
   
   let [loading,setLoading]=useState(false)
 
