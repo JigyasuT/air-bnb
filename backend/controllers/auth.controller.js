d906a38cd41137ee852616f23fe44800b68dc656
Jigyasu Tiwari <jigyasuti@gmail.com>
2026-02-12 10:29:51 +0530

    Fix secure cookie flag in auth controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -14,7 +14,7 @@ export const signUp=async (req,res)=>{
         let token=await genToken(user._id)
     res.cookie("token",token,{
      httpOnly:true,
-     secure:true",
+     secure:true,
      sameSite:"None",
      maxAge:7*24*60*60*1000
     })
